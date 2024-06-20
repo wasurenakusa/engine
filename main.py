28f70f542c0c0beb9a70a9febbd8a8228557a5ba
Christopher Schmitt <mail@christopher.gg>
2024-06-20 20:53:06 +0200

    Add the acctual starting plugin call (this is the only plugin type that is directly called by the engine!)

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 from pathlib import Path
 
 import typer
+from dotenv import load_dotenv
 
-from models.context import Context
-from models.payload import RequestPayload
 from plugin_system.plugin_manager import PluginManager
 from utilities.config_loader import load_character_config
+
+load_dotenv()
 
 
 def main(character_config_file: str) -> None:
     character_config = load_character_config(Path(character_config_file))
     pm = PluginManager(character_config)
 
-    # pm.load_and_register_plugins()
-
-    ctx = Context(
-        character=character_config,
-        request_payload=RequestPayload(),
-        workflow="test",
-        channel="test",
-        memory=[],
-        llm_functions=[],
-        system_prompts=[],
-    )
-    print(pm.call("generate_system_prompts", ctx=ctx).all())
+    pm.call("listen_to_channel").all()
 
 
 if __name__ == "__main__":
